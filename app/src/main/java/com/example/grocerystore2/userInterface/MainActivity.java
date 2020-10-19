7bdd8524b06ba60eddddfb14aa16197da6ce1120
Kevin Hlavaty <kdh6486@rit.edu>
2020-10-19 15:21:34 -0400

    Cleaned up Food class

--- app/src/main/java/com/example/grocerystore2/userInterface/MainActivity.java
+++ app/src/main/java/com/example/grocerystore2/userInterface/MainActivity.java
@@ -7,9 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.grocerystore2.R;
 
 public class MainActivity extends AppCompatActivity {
-    //hello there
-    //This is Jaron
-    //THis sia notehre acomment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
