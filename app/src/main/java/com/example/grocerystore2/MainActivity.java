75e79c45d005f0f33f0a633b9dfac9517355c356
Jaron Cummings <jkc5020@rit.edu>
2020-10-19 15:03:22 -0400

    This is annoying

--- app/src/main/java/com/example/grocerystore2/MainActivity.java
+++ app/src/main/java/com/example/grocerystore2/MainActivity.java
@@ -6,9 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     //hello there
+    //This is Jaron
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
